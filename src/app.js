ec1e123c3c46475cd44bf8524b783c21e17d0c45
Justice Rivera <142175716+justicetrace@users.noreply.github.com>
2024-06-06 00:21:44 +0000

    my excuse generator

--- src/app.js
+++ src/app.js
@@ -7,5 +7,30 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  let who = ["The dog", "My grandma", "The mailman", "My bird"];
+  const chosenWho = chooseRandomItem(who);
+
+  let action = ["ate", "peed", "crushed", "broke"];
+  const chosenAction = chooseRandomItem(action);
+
+  let what = ["my homework", "my phone", "the car"];
+  const chosenWhat = chooseRandomItem(what);
+
+  let when = [
+    "before the class",
+    "when I was sleeping",
+    "while I was exercising",
+    "during my lunch",
+    "while I was praying"
+  ];
+  const chosenWhen = chooseRandomItem(when);
+
+  const newHeader = `${chosenWho} ${chosenAction} ${chosenWhat} ${chosenWhen}`;
+  const h1 = document.querySelector("#excuse");
+  h1.textContent = newHeader;
+
+  function chooseRandomItem(array) {
+    const randomIndex = Math.floor(Math.random() * array.length);
+    return array[randomIndex];
+  }
 };
